ce57eb5c7fba27b5897b585f58f009551743e6e1
Kevin Disseldorp <k.disseldorp@ponthus.nl>
2023-06-05 11:24:59 +0200

    font aangepast

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -25,7 +25,7 @@ export default function Login() {
           <input type="password" id="password" className="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Password" />
         </div>
         <Link href="/home">
-        <button className="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full">Login</button>
+        <button className="bg-gradient-to-b from-gray-700 to-gray-900 font-semibold p-2 md:p-4 text-white w-full">Login</button>
         </Link>
         <Link href="/register">Don't have an account yet?  Sign Up</Link>
       </form>
