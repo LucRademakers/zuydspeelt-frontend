ae0e3ffe10ee5ef3eb35e500fdfd33a034549505
Aukje Reina van Montfort <1704095montfort@zuyd.nl>
2023-06-08 11:40:59 +0200

    Navigatieknop en achtergrondfoto, klopt nog niet

---
+++ app/gamesoverview.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const gamesoverview = () => {
+  const containerStyle = {
+    backgroundImage: "url('https://images.pexels.com/photos/3165335/pexels-photo-3165335.jpeg')",
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    /* Additional background styles here */
+  };
+
+  return (
+    <div style={containerStyle}>
+      {/* Content of the games overview page */}
+    </div>
+  );
+};
+export default gamesoverview;
