0f3260f262104ac440cdb0659741d01b4cbc2e9f
blood <jeffreyroering@gmail.com>
2023-06-10 14:07:14 +0200

    figma styling

--- app/searchbar/page.tsx
+++ app/searchbar/page.tsx
@@ -28,31 +28,42 @@ export default function Searchbar() {
 
   const handleGameClick = (game) => {
     console.log(`Clicked on game: ${game}`);
-    // Hier kan een link geplaatst worden naar de desbetreffende pagina van het spel.
   };
 
   return (
-    <div>
+    <div
+      style={{
+        width: '320px',
+        height: '86px',
+        left: '48px',
+        top: '213px',
+        filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+      }}
+    >
       <input
         type="text"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Search games..."
+        className="w-full px-4 py-2 text-gray-700 bg-gray-200 rounded-md focus:bg-white focus:outline-none"
       />
 
       {searchTerm && (
-
-      <ul>
-        {filteredGames.length > 0 ? (
-          filteredGames.map((game) => (
-            <li key={game} onClick={() => handleGameClick(game)}>
-              {game}
-            </li>
-          ))
-        ) : (
-          <li>No matching games found.</li>
-        )}
-      </ul>
+        <ul className="mt-4">
+          {filteredGames.length > 0 ? (
+            filteredGames.map((game) => (
+              <li
+                key={game}
+                onClick={() => handleGameClick(game)}
+                className="cursor-pointer py-1 hover:bg-gray-100"
+              >
+                {game}
+              </li>
+            ))
+          ) : (
+            <li>No matching games found.</li>
+          )}
+        </ul>
       )}
     </div>
   );
