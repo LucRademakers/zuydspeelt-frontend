646697dde7292b9456345e4a7257c5efb13081b0
Quentin <schaffhausen.qjj@gmail.com>
2023-06-11 23:32:54 +0200

    implemented dry principle

--- constants/apiConstants.tsx
+++ constants/apiConstants.tsx
@@ -1,6 +1,7 @@
+const API_BASE_URL = 'http://localhost:8080/api/';
+
 export enum API_ENDPOINT {
-    BASE_URL = 'http://localhost:8080/api/',
-    CATEGORIES = 'http://localhost:8080/api/categories',
-    USERS = 'http://localhost:8080/api/users',
-    GAMES = 'http://localhost:8080/api/games',
+    BASE_URL = API_BASE_URL,
+    CATEGORIES = API_BASE_URL + 'categories',
+    GAMES = API_BASE_URL + 'games',
 }
