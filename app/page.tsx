23f5438966e49039eeee67ea77654da88a08d22f
Bruce Jagessar <2103932jagessar@zuyd.nl>
2023-06-16 11:17:41 +0200

    16-06 11:17

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div style={{ display: 'flex', justifyContent: 'center', position: 'absolute', top:'0', zIndex: 100 }}>
+      <div>
         {<Searchbar />}
       </div>
 
--- app/searchbar/page.tsx
+++ app/searchbar/page.tsx
@@ -34,12 +34,21 @@ export default function Searchbar() {
   };
 
   return (
+  <div 
+  style ={{ display: 'flex', 
+  justifyContent: 'center', 
+  position: 'absolute', 
+  top:'0', 
+  zIndex: 100,
+  paddingBottom: 
+  }}
+  >
     <div
       style={{
         width: '320px',
         height: '86px',
         margin: '0 auto',
-        filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))'                            
+        filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
       }}
     >
       <input
@@ -72,6 +81,7 @@ export default function Searchbar() {
       </div>
       )}
       
+    </div>
     </div>
   );
 }
