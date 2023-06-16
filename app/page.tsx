b0003361e0bb134bd72884d0cad0cb27bd6e9427
Quentin <schaffhausen.qjj@gmail.com>
2023-06-16 22:52:55 +0200

    Moved dropdown to components folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import React from 'react';
-import Dropdown from "./dropdown/page";
+import Dropdown from "@/components/Dropdown";
 
 const Home = () => {
   return (
