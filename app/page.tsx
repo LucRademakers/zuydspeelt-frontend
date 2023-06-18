4e8a3f3b124e56eae12dc5f105968f378353d4c0
Danny Lao <1946110lao@zuyd.nl>
2023-06-18 16:15:53 +0200

    Dropdown + Navbar update

--- app/page.tsx
+++ app/page.tsx
@@ -4,37 +4,22 @@ import React from 'react';
 import Dropdown from "@/components/Dropdown";
 import Searchbar from "@/components/Searchbar";
 
+ 
+
+ 
+
+
 
 const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div style={{ position: 'absolute', top: '0', right: '0', width: '100px' }}>
-        <Dropdown />            
-      </div>
-      
       <Searchbar />
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed left-0 top-0 w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Hello world!
-        </div>
+    
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="/categories"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+         
         </div>
       </div>
 
