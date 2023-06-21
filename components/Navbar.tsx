d0e7df1c4d9abf1825ddc8b19074efeda7aeae58
MaxHorrichs93 <115566167+MaxHorrichs93@users.noreply.github.com>
2023-06-21 15:11:29 +0200

    Add files via upload

---
+++ components/Navbar.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+// installeer library icons: 'npm install @mui/icons-material'
+import { Attractions } from '@mui/icons-material'
+import { AppBar, Toolbar, IconButton, Typography, Stack, Button} from '@mui/material'
+import DropdownMenu from './Dropdown';
+
+// Zet hier je dropdown Items
+const categories = [
+    { value: 1, label: 'Action', path:'/Categories/Action' },
+    { value: 2, label: 'Puzzle', path:'/Categories/Puzzle'  },
+    { value: 3, label: 'Multiplayer', path:'/Categories/Multiplayer'  },
+    { value: 4, label: 'Race', path:'/Categories/Race'  },
+    { value: 5, label: 'Sport', path:'/Categories/Sport'  },
+  ];
+
+  
+
+ 
+
+
+export const Navbar = () => {
+
+
+    return (
+       <AppBar position='static' style={{ background: '#0881b5' }} sx={{ boxShadow: 3 }}>
+        <Toolbar>
+            <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
+                <Attractions/>
+            </IconButton>
+            <Typography variant='h6' component='div' sx={{flexGrow: 1}}>
+                ZUYDSPEELT
+            </Typography>
+            <Stack direction='row' spacing={2}>
+                {/* Zet hier je menu in de navbar. Verander de {options} met de naam die je de const had gegeven boveaan de pagina en geef de knop een naam */}
+                <DropdownMenu options={categories} buttonName='Categories'/>
+                <Button color='inherit'>Login</Button>
+
+            </Stack>
+        
+
+        </Toolbar>
+       </AppBar>
+    )
+}
