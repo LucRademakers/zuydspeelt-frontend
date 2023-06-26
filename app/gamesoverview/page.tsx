0c554a4df21ebb6c2b4389ae418de25bacd08a2e
Aukje Reina van Montfort <1704095montfort@zuyd.nl>
2023-06-26 16:15:25 +0200

    knopaanpassing

--- app/gamesoverview/page.tsx
+++ app/gamesoverview/page.tsx
@@ -41,7 +41,13 @@ export default function GamesOverview() {
           </button>
         </Link>
       </div>
-      <Navigation />
+      <div className="fixed bottom-20 right-20">
+      <Link href="gamesoverview/page2">
+        <button className="border border-gray-300 rounded-full py-2 px-4 bg-blue-500 text-white">
+          Page 2
+        </button>
+      </Link>
+      </div>
     </div>
   );
 }
