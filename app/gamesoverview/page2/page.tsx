49e16a4819760802b3f80211b4dc46972fadd48e
TheVioletRaven <80763755+TheVioletRaven@users.noreply.github.com>
2023-06-27 12:16:59 +0200

    kleine aanpassing layout in code page 2

--- app/gamesoverview/page2/page.tsx
+++ app/gamesoverview/page2/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default function page2 () {
+function page2 () {
   return (
     <div className="bg-blue-200 h-screen flex items-center justify-center">
       <h1 className="text-4xl text-gray-700">Page 2</h1>
+      <div className="fixed bottom-20 right-20">
+      <Link href="gamesoverview">
+        <button className="border border-gray-300 rounded-full py-2 px-4 bg-blue-500 text-white">
+          Page 1
+        </button>
+      </Link>
+      </div>
     </div>
+    
   );
 };
+
+export default page2
