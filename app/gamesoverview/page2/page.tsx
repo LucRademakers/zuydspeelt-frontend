de93a4963f00b0123c6e85ae1f2264313cfd4f29
TheVioletRaven <80763755+TheVioletRaven@users.noreply.github.com>
2023-06-27 12:01:15 +0200

    navigatie werkt

--- app/gamesoverview/page2/page.tsx
+++ app/gamesoverview/page2/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
 
-const page2 = () => {
+export default function page2 () {
   return (
     <div className="bg-blue-200 h-screen flex items-center justify-center">
       <h1 className="text-4xl text-gray-700">Page 2</h1>
     </div>
   );
 };
-
-export default page2;
