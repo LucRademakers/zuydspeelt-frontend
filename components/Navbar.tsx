b301a59933d4711b290b07d6415761815ae9afc2
Sander Lienaerts <sander@lienaerts.nl>
2023-06-28 23:43:31 +0200

    add examples using user state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
-
-// installeer library icons: 'npm install @mui/icons-material'
+import { useAuthenticatedUser } from "../app/authHandler";
 import Attractions from "@mui/icons-material/Attractions";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
@@ -8,23 +7,76 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import { API_ENDPOINT } from "../constants/apiConstants";
 
 import DropdownMenu from "./Dropdown";
 import Searchbar from "@/components/Searchbar";
 
+async function fetchCategories(user: any) {
+  if (user?.apiToken == null) {
+    return [];
+  }
+  let result = await fetch(API_ENDPOINT.CATEGORIES, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + user.apiToken,
+    },
+  });
+  return await result.json();
+}
+
+function CategoriesDropDown(props: any): JSX.Element {
+  const user = useAuthenticatedUser();
+  // log user
+  console.log(user);
+  let categories: any =
+    user != undefined
+      ? fetchCategories(user).then((result) =>
+          result.map((category: any) => {
+            return {
+              value: category.id,
+              label: category.name,
+              path: "/games/category/" + category.id,
+            };
+          })
+        )
+      : [{ value: 1, label: "Loading...", path: "#" }];
+
+  return <DropdownMenu options={categories} buttonName="Categories" />;
+}
+
+function LoginButton(props: any): JSX.Element {
+  if (useAuthenticatedUser() == undefined) {
+    return (
+      <Button color="inherit" href="/login">
+        Login
+      </Button>
+    );
+  }
+  return <></>;
+}
+
 // Zet hier je dropdown Items
-const categories = [
-  { value: 1, label: "Puzzle", path: "/" },
-  { value: 2, label: "Race", path: "/" },
-  { value: 3, label: "Action", path: "/" },
-];
 
 const games = [
   { value: 1, label: "Alex In Danger", path: "/games/alex-in-danger" },
-  { value: 2, label: "Fancy Pants Adventures", path: "/games/fancy-pants-adventures" },
-  { value: 3, label: "The Impossible Quiz", path: "/games/the-impossible-quiz" },
-  { value: 4, label: "The Worlds Hardest Game", path: "/games/the-worlds-hardest-game" },
-  { value: 5, label: "Tic Tac Toe", path: "/games/tic-tac-toe" }
+  {
+    value: 2,
+    label: "Fancy Pants Adventures",
+    path: "/games/fancy-pants-adventures",
+  },
+  {
+    value: 3,
+    label: "The Impossible Quiz",
+    path: "/games/the-impossible-quiz",
+  },
+  {
+    value: 4,
+    label: "The Worlds Hardest Game",
+    path: "/games/the-worlds-hardest-game",
+  },
+  { value: 5, label: "Tic Tac Toe", path: "/games/tic-tac-toe" },
 ];
 
 export const Navbar = () => {
@@ -35,7 +87,13 @@ export const Navbar = () => {
       sx={{ boxShadow: 3 }}
     >
       <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="logo" href="/">
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="logo"
+          href="/"
+        >
           <Attractions />
         </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -43,9 +101,9 @@ export const Navbar = () => {
         </Typography>
         <Stack direction="row" spacing={2}>
           {/* Zet hier je menu in de navbar. Verander de {options} met de naam die je de const had gegeven boveaan de pagina en geef de knop een naam */}
-          <DropdownMenu options={categories} buttonName="Categories" />
+          <CategoriesDropDown />
           <DropdownMenu options={games} buttonName="Games" />
-          <Button color="inherit">Login</Button>
+          <LoginButton />
         </Stack>
         <Searchbar />
       </Toolbar>
