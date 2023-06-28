12305e89a98547bc30be954154c95f6de51edc93
Senne Pieters <senne.pieters02@gmail.nl>
2023-06-28 17:25:07 +0200

    jest test file

---
+++ __tests__/actionleaderboard.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import Action from '@/app/leaderboard/action/page';
+
+describe('Action page', () => {
+  test('renders page title', () => {
+    render(<Action />);
+    const titleElement = screen.getByText('Action');
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test('renders navigation link to Home', () => {
+    render(<Action />);
+    const homeLinkElement = screen.getByText('Home');
+    expect(homeLinkElement).toBeInTheDocument();
+  });
+
+  test('renders search bar component', () => {
+    render(<Action />);
+    const searchbarComponent = screen.getByRole('search');
+    expect(searchbarComponent).toBeInTheDocument();
+  });
+
+  test('renders game rankings', () => {
+    render(<Action />);
+    const gameRankings = screen.getAllByRole('row');
+    expect(gameRankings).toHaveLength(41); // Total rows (including header rows) in all tables
+  });
+});
